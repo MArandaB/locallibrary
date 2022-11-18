634f66677831cd04ef431cf9660b34c0435819cd
MArandaB <mario_aranda2012@hotmail.com>
2022-11-18 12:35:25 +0100

    first commit

---
+++ catalog/urls.py
@@ -0,0 +1,22 @@
+# creamos urls.py en catalog
+from django.urls import path
+from .views import index, acerca_de, BookListView, BookDetailView, AuthorListView, AuthorDetailView, \
+             SearchResultsListView, LibrosPrestados, renovar_libro, \
+             AuthorCreate, AuthorUpdate, AuthorDelete 
+
+
+urlpatterns = [
+    path('',index, name ='index'),
+    path('acercade/', acerca_de, name='acercade'),
+    path('libros/', BookListView.as_view(), name='lista-libros'),
+    path('libros/<int:pk>', BookDetailView.as_view(), name='detalle-libro'),
+    path('autores/', AuthorListView.as_view(), name='lista-autores'),
+    path('autores/<int:pk>', AuthorDetailView.as_view(), name='detalle-autor'),
+    path('busqueda/', SearchResultsListView.as_view(), name='buscar'),
+    path('prestados/', LibrosPrestados.as_view(), name='prestados'),
+    path('libro/<uuid:pk>/renovar/', renovar_libro, name='renovar-fecha'),
+    path('autores/create/', AuthorCreate.as_view(), name='author-create'),
+    path('autores/<int:pk>/update/', AuthorUpdate.as_view(), name='author-update'),
+    path('autores/<int:pk>/delete/', AuthorDelete.as_view(), name='author-delete'),
+
+]
---
+++ catalog/views.py
@@ -0,0 +1,162 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from catalog.models import Book, Bookinstance, Author, Genre
+from django.views.generic import ListView, DetailView
+import datetime
+from catalog.forms import RenewBookForm, RenewBookModelForm
+from django.urls import reverse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+
+
+# Create your views here.
+def index_general(request):
+    
+
+    return render(request, 'index-general.html')
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books=Book.objects.all().count()
+    num_instances=Bookinstance.objects.all().count()
+
+    # Available books (status = 'a')
+    num_instances_available=Bookinstance.objects.filter(status='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors=Author.objects.count()  
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+    ultimoslibros = Book.objects.all().order_by('-id')[:10]
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'index.html',
+        context={
+            'num_books':num_books,
+            'num_instances':num_instances,
+            'num_instances_available':num_instances_available,
+            'num_authors':num_authors,
+            'num_visits':num_visits,
+            'ultimoslibros':ultimoslibros},
+    )
+
+##Listas genericas
+class BookListView(ListView):
+    '''Vista genérica para lista de libros'''
+    model = Book
+    paginate_by = 15
+    def get_queryset(self):
+        return Book.objects.all().order_by('title')
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context['ahora'] = datetime.datetime.now()
+        return context   
+
+##Vistas genericas
+class BookDetailView(DetailView):
+    '''Vista genérica para detalle de libro'''
+    model = Book
+
+
+class AuthorListView(ListView):
+    '''Vista genérica para lista de autores'''
+    model = Author
+    paginate_by = 15
+
+class AuthorDetailView(DetailView):
+    '''Vista genérica para detalle de autor'''
+    model = Author
+
+##Busqueda
+class SearchResultsListView(ListView):
+    model = Book
+    def get_queryset(self): # new
+        query = self.request.GET.get('q','')#voy a guardar query para el contexto
+        if query: 
+            self.query = query
+            return Book.objects.filter(title__icontains=query)
+        else:
+            return []    
+
+    def get_context_data(self, **kwargs):
+        context = super(SearchResultsListView, self).get_context_data(**kwargs)
+        context['busqueda'] = self.query
+        context ['anterior'] = self.request.META.get('HTTP_REFERER')
+        return context   
+
+class LibrosPrestados(ListView):
+    '''Vista genérica para lista de libros prestados'''
+    model = Bookinstance
+    template_name = 'catalog/libros_prestados.html'
+    paginate_by = 15
+    def get_queryset(self):
+        return Bookinstance.objects.filter(status__exact='o').order_by('due_back')  
+
+##Renovar libro
+def renovar_libro(request, pk):
+    book_instance = get_object_or_404(Bookinstance, pk = pk)
+    # If this is a POST request then process the Form data
+    if request.method == 'POST':
+
+        # Create a form instance and populate it with data from the request (binding):
+        form = RenewBookModelForm(request.POST)
+
+        # Check if the form is valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
+            book_instance.due_back = form.cleaned_data['due_back']
+            book_instance.status = form.cleaned_data['status']
+            book_instance.save()
+
+            # redirect to a new URL:
+            return HttpResponseRedirect(reverse('prestados'))
+
+    # If this is a GET (or any other method) create the default form.
+    else:
+    #inicializa la fecha de renovación dentro de 3 semanas
+        proposed_renewal_date = datetime.date.today() + \
+        datetime.timedelta(weeks=3)
+        form = RenewBookModelForm(initial={'due_back': proposed_renewal_date})
+
+    context = {
+        'form': form,
+        'book_instance': book_instance,
+    }
+
+    return render(request, 'catalog/renovacion_fecha.html', context)
+
+##Gestion de autores con vistas genéricas para
+class AuthorCreate(CreateView):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    ##si funciona bien: success url
+    success_url = reverse_lazy('lista-autores')
+    #initial = {'date_of_death': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__' # Not recommended (potential security issue if more fields added)
+    success_url = reverse_lazy('lista-autores')
+
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('lista-autores')
+
+
+def acerca_de(request):
+    context = {}
+    context['title'] = 'Acerca de'
+    context['coords'] = "41.6447242,-0.9231553"
+    
+    return render(request, 'acerca_de.html',context)
